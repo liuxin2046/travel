9e2979bc12b8ce43d02d2827900be69ad4cf00b0
Michael Liu <18279570273@163.com>
2019-06-17 21:06:39 +0800

    add probblem

--- src/pages/detail/components/Header.vue
+++ src/pages/detail/components/Header.vue
@@ -25,6 +25,7 @@ export default {
   },
   methods: {
     handleScroll () {
+    console.log('scroll')
       const top = document.documentElement.scrollTop
       if (top > 60) {
         let opacity = top / 140
@@ -38,6 +39,9 @@ export default {
   },
   activated () {
     window.addEventListener('scroll',this.handleScroll)
+  },
+  deactivated () {
+    window.removeEventListener('scroll',this.handleScroll)
   }
 }
 </script>
